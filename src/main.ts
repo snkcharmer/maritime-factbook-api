ec8a3748daf197e2f2e27fc26ba72437e38cd824
Israel Anthony Lagunzad <isanclagunzad@gmail.com>
2024-11-13 00:22:01 +0800

    🎨updated main.ts configuration, enabled API versioning, enabled Cors

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const globalPrefix = 'api/v1';
+
+  app.setGlobalPrefix(globalPrefix, {});
 
   const config = new DocumentBuilder()
-    .setTitle('My API')
-    .setDescription('The API documentation')
-    .setVersion('1.0')
-    .addTag('auth')
+    .setTitle(process.env.API_TITLE || '')
+    .setDescription(process.env.API_DESCRIPTION || '')
+    .setVersion(process.env.API_VERSION || '')
     .build();
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-docs', app, document);
+
+  app.enableVersioning({
+    type: VersioningType.URI, // Versioning based on URL (e.g., api/v1/)
+    defaultVersion: '1',
+  });
+
+  app.enableCors({});
 
   await app.listen(3333);
 }
